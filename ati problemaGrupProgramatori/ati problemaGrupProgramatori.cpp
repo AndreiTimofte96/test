b76202fd1e72e010469b854a2d4db0666f990091
Timofte Andrei Teodor <andrei.timofte96@gmail.com>
2017-12-01 22:59:34 +0200

    Add project files.

---
+++ ati problemaGrupProgramatori/ati problemaGrupProgramatori.cpp
@@ -0,0 +1,41 @@
+// ati problemaGrupProgramatori.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#define DMAX 201
+
+
+using namespace std;
+
+ifstream fin("date.in");
+
+int v[DMAX], sum = 0, min = 99999999, elem;
+
+int main()
+{
+	int  x, n, i, j;
+
+	fin >> n;
+	for (i = 0; i < n; i++) {
+		fin >> v[i];
+	}
+
+	for (i = 0; i < n; i++) {
+		x = v[i];
+		sum = 0;
+		for (j = 0; j < n; j++) {
+			sum += abs(v[j] - x);
+		}
+		if (sum < min) {
+			min = sum;
+			
+			elem = x;
+		}
+	}
+
+	cout << elem;
+	return 0;
+}
+
